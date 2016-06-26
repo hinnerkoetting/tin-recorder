b82b98b46043e8335db7968b8ce97ff6126d0daa
Hinnerk Oetting <hinnerk.oetting@gmail.com>
2016-06-26 08:21:03 +0200

    Basic error handling

--- app/main.js
+++ app/main.js
@@ -63,5 +63,11 @@ function analyseParameter() {
 
 analyseParameter();
 
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
+process.on('uncaughtException', (err) => {
+  console.log(`Caught exception: ${err}`);
+  mainWindow.webContents.send('showError("foo");');
+});
+
+var ipc = require('electron').ipcMain;
+
+ipc.on('onError', () => mainWindow.webContents.openDevTools());
